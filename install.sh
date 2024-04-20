6598064e5cb1d5b2144d786eb055adb52ac7e409
Paulo Truta <ptruta@pm.me>
2024-04-20 13:32:21 +0200

    Fixes to installer script, added manual yq installation

--- install.sh
+++ install.sh
@@ -44,6 +44,11 @@ then
   VERBOSE_MODE="true"
 fi
 
+if [[ "${arguments}" = *"--skip-prompt"* ]]
+then
+  SKIP_PROMPT="true"
+fi
+
 if [[ "${arguments}" = *"--no-install-avahi"* ]]
 then
   INSTALL_AVAHI="false"
@@ -223,9 +228,27 @@ install_docker_compose() {
 }
 
 install_yq() {
-    # pip3 -v install yq
-    sudo apt-get install --yes yq
+    declare -A yq_sha256
+    yq_sha256["arm64"]="8879e61c0b3b70908160535ea358ec67989ac4435435510e1fcb2eda5d74a0e9"
+    yq_sha256["amd64"]="c93a696e13d3076e473c3a43c06fdb98fafd30dc2f43bc771c4917531961c760"
+
+    yq_version="v4.24.5"
+    system_arch=$(dpkg --print-architecture)
+    yq_binary="yq_linux_${system_arch}"
+
+    yq_temp_file="/tmp/yq"
+    curl -L "https://github.com/mikefarah/yq/releases/download/${yq_version}/${yq_binary}" -o "${yq_temp_file}"
+
+    if [[ "$(sha256sum "${yq_temp_file}" | awk '{ print $1 }')" == "${yq_sha256[$system_arch]}" ]]; then
+        sudo mv "${yq_temp_file}" /usr/bin/yq
+        sudo chmod +x /usr/bin/yq
+        echo "yq installed successfully..."
+    else
+        echo "yq install failed. sha256sum mismatch"
+        exit 1
+    fi
 }
+
 
 install_cloudflared() {
     wget -q https://github.com/cloudflare/cloudflared/releases/download/2023.3.1/cloudflared-linux-$(printenv ARCH).deb
